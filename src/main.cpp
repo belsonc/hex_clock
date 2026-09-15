183ea0cf5152aee6b1890758c839d383b965c63e
belsonc <craig.belson@gmail.com>
2026-09-15 10:31:21 -0400

    initial good build - color displaying on screen, time displaying on screen, fake ssid/password in place

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,23 @@
 #include <lvgl.h>
 #include "touch.h"
 #include <WiFi.h>
+#include <time.h>
+#include <lv_conf.h>
 
-const char* ssid = "FakeSSID";         
-const char* password = "FakePassword"; 
+//wifi connection info
+const char* ssid = "Fake ssid";         
+const char* password = "fake password"; 
+
+
+// NTP Server Settings
+const char* ntpServer = "pool.ntp.org";
+// Time zone offsets in seconds (e.g., Eastern Time: -5 hours * 3600 = -18000)
+const long  gmtOffset_sec = -18000; 
+const int   daylightOffset_sec = 3600; // 1 hour for Daylight Saving Time
+int hour, minute, second;
+int hour_color, minute_color, second_color;
+String hour_str, minute_str, second_str, time_str;
+lv_color_t text_color = lv_color_hex(0x810226);
 
 // Display backlight pin
 #define GFX_BL 38
@@ -54,10 +68,88 @@ questions -
 - how often should i update the display?  since i'm doing it locally, it's not like i'm going to be hammering some server, etc.*/
 
 
+// Display flushing callback for LVGL
+void my_disp_flush(lv_display_t *disp, const lv_area_t *area, uint8_t *px_map)
+{
+  uint32_t w = (area->x2 - area->x1 + 1);
+  uint32_t h = (area->y2 - area->y1 + 1);
+
+#if (LV_COLOR_16_SWAP != 0)
+  gfx->draw16bitBeRGBBitmap(area->x1, area->y1, (uint16_t *)px_map, w, h);
+#else
+  gfx->draw16bitRGBBitmap(area->x1, area->y1, (uint16_t *)px_map, w, h);
+#endif
+
+  lv_disp_flush_ready(disp);
+}
+
+
+// Tick callback for LVGL
+static uint32_t my_tick(void)
+{
+  return millis();
+}
+
 void setup()
 {
     Serial.begin(115200);
     delay(1000); // Give the serial monitor time to connect
+    touch_init();
+
+    // Initialize 9-bit mode SPI
+    bus = new Arduino_ESP32SPI(
+        GFX_NOT_DEFINED /* DC */, 39 /* CS */, 48 /* SCK */, 47 /* MOSI */, GFX_NOT_DEFINED /* MISO */);
+
+  // Initialize RGB panel (hardware specific)
+    Arduino_ESP32RGBPanel *rgbpanel = new Arduino_ESP32RGBPanel(
+        18 /* DE */, 17 /* VSYNC */, 16 /* HSYNC */, 21 /* PCLK */,
+    #if 0
+        4 /* R0 */, 5 /* R1 */, 6 /* R2 */, 7 /* R3 */, 15 /* R4 */,
+        8 /* G0 */, 20 /* G1 */, 3 /* G2 */, 46 /* G3 */, 9 /* G4 */, 10 /* G5 */,
+        11 /* B0 */, 12 /* B1 */, 13 /* B2 */, 14 /* B3 */, 0 /* B4 */,
+    #else
+        11 /* R0 */, 12 /* R1 */, 13 /* R2 */, 14 /* R3 */, 0 /* R4 */,
+        8 /* G0 */, 20 /* G1 */, 3 /* G2 */, 46 /* G3 */, 9 /* G4 */, 10 /* G5 */,
+        4 /* B0 */, 5 /* B1 */, 6 /* B2 */, 7 /* B3 */, 15 /* B4 */,
+    #endif
+        1 /* hsync_polarity */, 10 /* hsync_front_porch */, 8 /* hsync_pulse_width */, 50 /* hsync_back_porch */,
+        1 /* vsync_polarity */, 10 /* vsync_front_porch */, 8 /* vsync_pulse_width */, 20 /* vsync_back_porch */);
+
+  // Initialize display panel
+    gfx = new Arduino_RGB_Display(
+        480 /* width */, 480 /* height */, rgbpanel, 0 /* rotation */, true /* auto_flush */,
+        bus, GFX_NOT_DEFINED /* RST */, st7701_4848s040_init_operations, sizeof(st7701_4848s040_init_operations));
+
+
+  // Check if display initialization succeeded
+    if (!gfx->begin())
+    {
+        Serial.println("gfx->begin() failed!");
+    }
+
+    // Turn on backlight
+    #ifdef GFX_BL
+        pinMode(GFX_BL, OUTPUT);
+        analogWrite(GFX_BL, TFT_BRIGHTNESS);
+    #endif
+
+    //turn the display itself on
+    gfx->displayOn();
+
+
+    // Initialize LVGL
+    lv_init();
+
+    // Set tick source for LVGL timing
+    lv_tick_set_cb(my_tick);
+
+    // Create LVGL display
+    display = lv_display_create(TFT_HOR_RES, TFT_VER_RES);
+    lv_display_set_flush_cb(display, my_disp_flush);
+    lv_display_set_buffers(display, draw_buf, NULL, sizeof(draw_buf), LV_DISPLAY_RENDER_MODE_PARTIAL);
+    lv_display_set_rotation(display, TFT_ROTATION);
+
+
 
     Serial.println("\nConnecting to Wi-Fi...");
     WiFi.begin(ssid, password);
@@ -71,8 +163,59 @@ void setup()
     Serial.println("\nWi-Fi Connected!");
     Serial.print("IP Address: ");
     Serial.println(WiFi.localIP());
+
+    // Initialize NTP
+    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+    Serial.println("NTP Initialized!");
+    struct tm timeinfo;
+    if(!getLocalTime(&timeinfo))
+    {
+        Serial.println("Failed to obtain time");
+        return;
+    }
+    Serial.println("Time obtained successfully!");
+    hour = timeinfo.tm_hour;
+    minute = timeinfo.tm_min;
+    second = timeinfo.tm_sec;
+
+    hour_color = hour * 256 / 24; // Scale hour to 0-255
+    minute_color = minute * 256 / 60; // Scale minute to 0-255
+    second_color = second * 256 / 60; // Scale second to 0-255
+    Serial.println("Got RGB");
+    lv_color_t bg_color = lv_color_make(hour_color, minute_color, second_color);
+    lv_obj_set_style_bg_color(lv_screen_active(), bg_color, LV_PART_MAIN);
+
+    lv_obj_t *label = lv_label_create(lv_screen_active());
+
+    if (hour < 10)
+        hour_str = "0" + String(hour);
+    else
+        hour_str = String(hour);
+
+    if (minute < 10)
+        minute_str = "0" + String(minute);
+    else
+        minute_str = String(minute);
+
+    if (second < 10)
+        second_str = "0" + String(second);
+    else
+        second_str = String(second);
+
+    time_str = hour_str + ":" + minute_str + ":" + second_str;
+
+    lv_label_set_text(label, time_str.c_str());
+    lv_obj_set_style_text_color(label, text_color, LV_PART_MAIN);
+    lv_obj_set_style_text_font(label, &lv_font_montserrat_24, LV_PART_MAIN);
+    lv_obj_align(label, LV_ALIGN_CENTER, 90, 90);
+
+
+    Serial.println("Background color set based on time!");
+    Serial.println("RGB Calculated Successfully");
 }
 
 void loop()
 {
+    lv_timer_handler();
+    delay(5);
 }
